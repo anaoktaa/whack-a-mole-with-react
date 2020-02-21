a4ae8c613d4e230d14485850e87284bd5de85871
@anaokta <ana.voltras@co.id>
2020-02-21 16:24:50 +0700

    improvement

--- whack-a-mole/src/App.js
+++ whack-a-mole/src/App.js
@@ -6,16 +6,9 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      isShowMole: true,
       active: null,
       isStart: false,
       isEndGame: true,
-      mole0: false,
-      mole1: false,
-      mole2: false,
-      mole3: false,
-      mole4: false,
-      mole5: false,
       score: 0
     }
   }
@@ -37,47 +30,17 @@ class App extends React.Component {
 
 
   showTikus = () => {
+    const tikus = document.querySelectorAll('.tikus');
     if (!this.state.isEndGame) {
       const randomTime = this.randomWaktu(200, 400)
-      const setFalseRandomTime =  1000
+      const setHideMole =  1000
       setTimeout(() => {
-        let a = this.randomTanah()
-        this.setState({active: a})
-        if (a === 0) {
-          this.setState({mole0: true})
-          setTimeout(() => {this.setState({mole0: false})}, setFalseRandomTime)
-        }
-        if (a === 1) {
-          this.setState((prevState) => {
-            return {mole1: true,}
-          })
-          setTimeout(() => {this.setState({mole1: false})}, setFalseRandomTime)
-        }
-        if (a === 2) {
-          this.setState((prevState) => {
-            return {mole2: true,}
-          })
-          setTimeout(() => {this.setState({mole2: false})}, setFalseRandomTime)
-        }
-        if (a === 3) {
-          this.setState((prevState) => {
-            return {mole3: true,}
-          })
-          setTimeout(() => {this.setState({mole3: false})}, setFalseRandomTime)
-        }
-        if (a === 4) {
-          this.setState((prevState) => {
-            return {mole4: true,}
-          })
-          setTimeout(() => {this.setState({mole4: false})}, setFalseRandomTime)
-        }
-        if (a === 5) {
-          this.setState((prevState) => {
-            return {mole5: true,}
-          })
-          setTimeout(() => {this.setState({mole5: false})}, setFalseRandomTime)
-        }
-          this.showTikus()
+        let index = this.randomTanah()
+        tikus[index].classList.add('muncul');
+        setTimeout(() => {
+          tikus[index].classList.remove('muncul');
+        }, setHideMole);
+        this.showTikus()
       }, randomTime)
     }
     else {
@@ -97,30 +60,18 @@ class App extends React.Component {
   }
 
    handlePukul = (mole) => {
-     if (this.state.mole0 && mole === "mole0") {
-      this.setState((prevState) => {return {score: prevState.score+1, mole0: false}})
-     }
-     if (this.state.mole1 && mole === "mole1") {
-      this.setState((prevState) => {return {score: prevState.score+1, mole1: false}})
-     }
-     if (this.state.mole2 && mole === "mole2") {
-      this.setState((prevState) => {return {score: prevState.score+1, mole2: false}})
-     }
-     if (this.state.mole3 && mole === "mole3") {
-      this.setState((prevState) => {return {score: prevState.score+1, mole3: false}})
-     }
-     if (this.state.mole4 && mole === "mole4") {
-      this.setState((prevState) => {return {score: prevState.score+1, mole4: false}})
-     }
-     if (this.state.mole5 && mole === "mole5") {
-      this.setState((prevState) => {return {score: prevState.score+1, mole5: false}})
-     }
+    const tikus = document.querySelectorAll('.tikus');
+    if (tikus[mole].className === "tikus muncul") {
+      this.setState((prevState) => {
+        return {score: prevState.score+1}
+      })
+      tikus[mole].classList.remove('muncul');
+    }
    }
 
 
-   
   render() {
-    const {isStart, mole0, mole1, mole2, mole3, mole4, mole5, score} = this.state
+    const {isStart, score} = this.state
     return (
       <div className="container">
         <div className="row">
@@ -141,25 +92,25 @@ class App extends React.Component {
       
    
         <div className="item-container"> 
-          <div className="tanah" onClick={() => this.handlePukul("mole0")}>
-              <div className={mole0? "tikus muncul" :"tikus"}></div>            
+          <div className="tanah" onClick={() => this.handlePukul(0)}>
+              <div className="tikus"></div>            
           </div>
-          <div className="tanah" onClick={() => this.handlePukul("mole1")}>
-              <div className={mole1? "tikus muncul" :"tikus"}></div>
+          <div className="tanah" onClick={() => this.handlePukul(1)}>
+              <div className="tikus"></div>
           </div>
-          <div className="tanah" onClick={() => this.handlePukul("mole2")}>
-              <div className={mole2? "tikus muncul" :"tikus"}></div>
+          <div className="tanah" onClick={() => this.handlePukul(2)}>
+              <div className="tikus"></div>
           </div>
         </div>
         <div className="item-container"> 
-          <div className="tanah" onClick={() => this.handlePukul("mole3")}>
-              <div className={mole3? "tikus muncul" :"tikus"}></div>
+          <div className="tanah" onClick={() => this.handlePukul(3)}>
+              <div className="tikus"></div>
           </div>
-          <div className="tanah" onClick={() => this.handlePukul("mole4")}>
-              <div className={mole4? "tikus muncul" :"tikus"}></div>
+          <div className="tanah" onClick={() => this.handlePukul(4)}>
+              <div className="tikus"></div>
           </div>
-          <div className="tanah" onClick={() => this.handlePukul("mole5")}>
-              <div className={mole5? "tikus muncul" :"tikus"}></div>
+          <div className="tanah" onClick={() => this.handlePukul(5)}>
+              <div className="tikus"></div>
           </div>
         </div>
       </div>
