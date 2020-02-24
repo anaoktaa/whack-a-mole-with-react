317edca359dcee051a38fc0671ad5a51e4a3a778
@anaokta <ana.voltras@co.id>
2020-02-24 15:26:10 +0700

    Updates

---
+++ precache-manifest.24d161ad8c296c0cca499e267b0b3ca3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "081618c3037f0431eb26ac51bc8f80c2",
+    "url": "/whack-a-mole-with-react/index.html"
+  },
+  {
+    "revision": "361129168e6d7ae6c6fe",
+    "url": "/whack-a-mole-with-react/static/css/main.7810a78a.chunk.css"
+  },
+  {
+    "revision": "7b142f9e532164e6d05e",
+    "url": "/whack-a-mole-with-react/static/js/2.fd389187.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/whack-a-mole-with-react/static/js/2.fd389187.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "361129168e6d7ae6c6fe",
+    "url": "/whack-a-mole-with-react/static/js/main.1e10670b.chunk.js"
+  },
+  {
+    "revision": "78824b553629cd044214",
+    "url": "/whack-a-mole-with-react/static/js/runtime-main.998a4489.js"
+  },
+  {
+    "revision": "779caa76cbfc027a09b365a1a871a24f",
+    "url": "/whack-a-mole-with-react/static/media/GloriaHallelujah-Regular.779caa76.ttf"
+  },
+  {
+    "revision": "938a0913e59c818186f3663e2aa9eeb5",
+    "url": "/whack-a-mole-with-react/static/media/dirt.938a0913.svg"
+  },
+  {
+    "revision": "33f3e860a3b9964534797a71e3285347",
+    "url": "/whack-a-mole-with-react/static/media/mole.33f3e860.svg"
+  }
+]);
